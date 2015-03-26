9027899824a40c25b2fa5d243af88516e1180807
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2015-03-26 15:18:32 +0300

    [pidgin-skype] Moved to Github.

--- pidgin/pidgin-skype/build.sh
+++ pidgin/pidgin-skype/build.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
 
 PKG_EPOCH='2'
-SRC_URL='http://skype4pidgin.googlecode.com/svn/trunk'
-DEPENDS='subversion'
+SRC_URL='http://github.com/EionRobb/skype4pidgin.git'
 . "$(dirname "$0")/../commons.sh"
-REV=''
 
 version() {
-    if [ -z "$REV" ]
-    then
-        local rev="$(_svn_rev)"
-    else
-        local rev="$REV"
-    fi
-
-    local delta='0'
-    local version=$(svn cat -r $rev "$SRC_DIR/libskypekit/libskypekit.h" | awk -F '"' '/\/\* version \*\// {print $2}')
-    _pkg_version "$version" "$delta" "$rev" "r$rev"
-}
-
-update() {
-    _svn_update "$SRC_URL"
-}
-
-_changelog() {
-    local cur_version="$(_cur_version "$1")"
-    _svn_changelog "${cur_version##*~}" "$REV" "$SRC_DIR" "$RELATIVE_SRC_DIR"
-}
-
-_checkout() {
-    local dest="$1"
-    _svn_checkout "$dest" "$SRC_DIR"
+    local delta='9'
+    local version=$(git --git-dir="$SRC_DIR/.git" show $REV:libskypekit/libskypekit.h | awk -F '"' '/\/\* version \*\// {print $2}')
+    _pkg_version "$version" "$delta"
 }
 
 _main $@
--- pidgin/pidgin-skypeweb/build.sh
+++ pidgin/pidgin-skypeweb/build.sh
@@ -1,41 +1,14 @@
 #!/bin/bash
 
-SRC_URL='http://skype4pidgin.googlecode.com/svn/trunk'
+SRC_URL='http://github.com/EionRobb/skype4pidgin.git'
 SRC_DIR_NAME='pidgin-skype'
 RELATIVE_SRC_DIR='skypeweb'
-DEPENDS='subversion'
 . "$(dirname "$0")/../commons.sh"
-REV=''
-
-_rev() {
-    if [ -z "$REV" ]
-    then
-        _svn_rev "$SRC_DIR/$RELATIVE_SRC_DIR"
-    else
-        echo "$REV"
-    fi
-}
 
 version() {
-    local rev="$(_rev)"
-    local delta='0'
-    local version=$(svn cat -r $rev "$SRC_DIR/skypeweb/libskypeweb.h" | awk -F '"' '/#define SKYPEWEB_PLUGIN_VERSION/ {print $2}')
-    _pkg_version "$version" "$delta" "$rev" "r$rev"
-}
-
-update() {
-    _svn_update "$SRC_URL"
-}
-
-_changelog() {
-    local rev="$(_rev)"
-    local cur_version="$(_cur_version "$1")"
-    _svn_changelog "${cur_version##*~}" "$rev" "$SRC_DIR" "$RELATIVE_SRC_DIR"
-}
-
-_checkout() {
-    local dest="$1"
-    _svn_checkout "$dest" "$SRC_DIR" "$RELATIVE_SRC_DIR"
+    local delta='9'
+    local version=$(git --git-dir="$SRC_DIR/.git" show $REV:skypeweb/libskypeweb.h | awk -F '"' '/#define SKYPEWEB_PLUGIN_VERSION/ {print $2}')
+    _pkg_version "$version" "$delta"
 }
 
 _main $@

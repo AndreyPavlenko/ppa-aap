b02fb23b15ec7ce50409b849cb794adf54e46e64
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2015-04-25 10:44:04 +0300

    [iptvsimple] Bump version

--- kodi/kodi-addon-iptvsimple/build.sh
+++ kodi/kodi-addon-iptvsimple/build.sh
@@ -4,7 +4,7 @@
 . "$(dirname "$0")/../commons.sh"
 
 version() {
-    local delta='0'
+    local delta='2'
     local v_major=$(git --git-dir="$SRC_DIR/.git" show $REV:configure.in | grep -E 'm4_define\(\[MAJOR\]' | awk '{print $2}' | tr -d ')')
     local v_minor=$(git --git-dir="$SRC_DIR/.git" show $REV:configure.in | grep -E 'm4_define\(\[MINOR\]' | awk '{print $2}' | tr -d ')')
     local v_micro=$(git --git-dir="$SRC_DIR/.git" show $REV:configure.in | grep -E 'm4_define\(\[MICRO\]' | awk '{print $2}' | tr -d ')')

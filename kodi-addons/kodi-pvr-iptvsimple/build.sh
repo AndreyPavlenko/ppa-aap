74667652d49c3285a81356e995206adb39d78a29
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2015-12-21 23:49:28 +0300

    Added new repo - kodi-addons

---
+++ kodi-addons/kodi-pvr-iptvsimple/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+: ${SRC_URL:='https://github.com/AndreyPavlenko/pvr.iptvsimple.git'}
+PPA_DEPENDS="deb http://ppa.launchpad.net/team-xbmc/unstable/ubuntu #DISTRIB# main|\
+deb http://ppa.launchpad.net/team-xbmc/xbmc-ppa-build-depends/ubuntu #DISTRIB# main"
+. "$(dirname "$0")/../commons.sh"
+
+version() {
+    local delta='0'
+    local version=$(git --git-dir="$SRC_DIR/.git" show $REV:src/client.h | awk '/#define PVR_CLIENT_VERSION/ {print $3}' | tr -d '"')
+    echo "$(_pkg_version ${version} $delta)"
+}
+
+_deb_dir() {
+    cp -r "$1/debian" "$BUILD_DIR"
+    cp -r "$DIR/debian" "$BUILD_DIR"
+    echo "$BUILD_DIR/debian"
+}
+
+_main $@

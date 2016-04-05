8723affa88bab74c4115382dabdae2a6f6ef7173
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2016-04-05 22:49:56 +0300

    New ppa - kodi-addons-testing

---
+++ kodi-addons-testing/kodi-pvr-iptvsimple/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+: ${SRC_URL:='https://github.com/AndreyPavlenko/pvr.iptvsimple.git'}
+. "$(dirname "$0")/../commons.sh"
+
+version() {
+    local delta='1'
+    local version=$(git --git-dir="$SRC_DIR/.git" show $REV:src/client.h | awk '/#define PVR_CLIENT_VERSION/ {print $3}' | tr -d '"')
+    echo "$(_pkg_version ${version} $delta)"
+}
+
+_deb_dir() {
+    cp -r "$1/debian" "$BUILD_DIR"
+    cp -r "$DIR/debian" "$BUILD_DIR"
+    echo "$BUILD_DIR/debian"
+}
+
+_main $@

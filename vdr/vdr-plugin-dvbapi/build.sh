f465ea8cc336392b02690188ddb5a185330e8c73
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2016-09-09 21:16:00 +0300

    [vdr-plugin-dvbapi] Fixed armhf build

--- vdr/vdr-plugin-dvbapi/build.sh
+++ vdr/vdr-plugin-dvbapi/build.sh
@@ -4,7 +4,7 @@ SRC_URL='https://github.com/manio/vdr-plugin-dvbapi.git'
 . "$(dirname "$0")/../commons.sh"
 
 version() {
-    local delta='9'
+    local delta='14'
     local version=$(git --git-dir="$SRC_DIR/.git" show $REV:DVBAPI.h | awk '/#define VERSION / {print $3}' | tr -d '[";]')
     _pkg_version "$version" "$delta"
 }

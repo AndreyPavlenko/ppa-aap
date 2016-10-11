dd10a5681ffd3518fff51ea07871752038e177f3
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2016-10-11 21:11:48 +0300

    [iptvsimple] Fixed build

--- kodi-addons-testing/commons.sh
+++ kodi-addons-testing/commons.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 : ${PPA_DIR:="$(cd "$(dirname "$BASH_SOURCE")" && pwd)"}
-PPA_DEPENDS="deb http://ppa.launchpad.net/team-xbmc/xbmc-nightly/ubuntu #DISTRIB# main|\
-deb http://ppa.launchpad.net/team-xbmc/xbmc-ppa-build-depends/ubuntu #DISTRIB# main"
+PPA_DEPENDS="deb http://ppa.launchpad.net/team-xbmc/xbmc-nightly/ubuntu #DISTRIB# main"
 . "$PPA_DIR/../commons.sh"
